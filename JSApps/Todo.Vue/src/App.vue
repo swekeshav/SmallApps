9ff23f12bc4654ca2ecebef088db99802c2119cb
Keshav Sharma <sharma.keshav39@live.com>
2022-12-21 22:58:04 +0530

    Add SplitPanes nuget package

--- JSApps/Todo.Vue/src/App.vue
+++ JSApps/Todo.Vue/src/App.vue
@@ -1,27 +1,29 @@
 <template>
-    <Suspense>
-        <TodoPage />
-    </Suspense>
+  <!-- <Suspense>
+    <TodoPage />
+  </Suspense> -->
+  <SplitPanes />
 </template>
 
 <script>
-    import TodoPage from './components/TodoPage.vue'
+// import TodoPage from "./components/TodoPage.vue";
+import SplitPanes from "./components/SplitPanes.vue";
 
-    export default {
-        name: 'App',
-        components: {
-            TodoPage
-        }
-    }
+export default {
+  name: "App",
+  components: {
+    // TodoPage,
+    SplitPanes,
+  },
+};
 </script>
 
 <style>
-    #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 </style>
