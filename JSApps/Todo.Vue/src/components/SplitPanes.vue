a6bebd6d15b31f279442fda1a1c02b09b5596b77
Keshav Sharma <sharma.keshav39@gmail.com>
2022-12-21 22:58:38 +0530

    Add SplitPanes nuget package (#11)
    Co-authored-by: Keshav Sharma <sharma.keshav39@live.com>

---
+++ JSApps/Todo.Vue/src/components/SplitPanes.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { Splitpanes, Pane } from "splitpanes";
+import "splitpanes/dist/splitpanes.css";
+</script>
+<template>
+  <splitpanes class="default-theme" style="height: 100vh">
+    <pane min-size="20" max-size="70">
+      <!-- <img src="../assets/images/mountains.jpg" alt="Mountains" /> -->
+      1
+    </pane>
+    <pane min-size="20" max-size="70">
+      <!-- <img src="../assets/images/mountains.jpg" alt="Mountains" /> -->
+      2
+    </pane>
+  </splitpanes>
+</template>
+<style>
+.splitpanes__pane {
+  background-image: url("../assets/images/mountains.jpg");
+}
+</style>
