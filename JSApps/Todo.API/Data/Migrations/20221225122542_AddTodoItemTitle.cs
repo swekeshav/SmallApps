64f78ef6086312c39d3e40181dd5bc639925ff56
Keshav Sharma <sharma.keshav39@gmail.com>
2022-12-25 18:08:53 +0530

    Add TodoItem object (#14)
    Co-authored-by: Keshav Sharma <sharma.keshav39@live.com>

---
+++ JSApps/Todo.API/Data/Migrations/20221225122542_AddTodoItemTitle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Todo.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTodoItemTitle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Completed",
+                table: "TodoItems",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "TodoItems",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "TodoItems",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Completed",
+                table: "TodoItems");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "TodoItems");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "TodoItems");
+        }
+    }
+}
